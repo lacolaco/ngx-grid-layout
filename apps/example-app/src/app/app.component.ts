e896016f32469180bd7b81ed02e9403460d727dd
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-03-08 11:29:48 +0900

    fix

--- apps/example-app/src/app/app.component.ts
+++ apps/example-app/src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 }
