ea671d0ed84f0b1209c14bf624dd728cdee3bde1
Suguru Inatomi <suguru.inatomi@gmail.com>
2019-03-08 11:32:29 +0900

    build: remove ngx-store (#10)
    * build: remove ngx-store
    
    * fix
    
    * fix

--- apps/example-app/src/app/app.component.ts
+++ apps/example-app/src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@lacolaco/reactive-store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  count$ = this.store.select(state => state.count);
-  constructor(private store: Store<any>) {}
-
-  ngOnInit() {
-    setInterval(() => {
-      this.store.patch(state => ({ count: (state.count || 0) + 1 }));
-    }, 1000);
-  }
+export class AppComponent {
 }
